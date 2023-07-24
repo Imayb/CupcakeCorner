aa423a950bf24fe3fe55b707ef16a3c29ca09737
Imayb <17imayberry@gmail.com>
2023-07-24 15:19:25 -0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ivana Mayberry on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
